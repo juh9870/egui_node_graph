e61ff2aafa748f7ff96928a7894198594bdaed5d
juh9870 <juh9870>
2023-10-30 01:08:51 +0200

    chore: derive Debug for NodeFinder

--- egui_node_graph/src/node_finder.rs
+++ egui_node_graph/src/node_finder.rs
@@ -4,7 +4,7 @@ use crate::{color_hex_utils::*, CategoryTrait, NodeTemplateIter, NodeTemplateTra
 
 use egui::*;
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 #[cfg_attr(feature = "persistence", derive(serde::Serialize, serde::Deserialize))]
 pub struct NodeFinder<NodeTemplate> {
     pub query: String,
